19865e9ec9383cff8d3632f8222ac3021804c5e4
himanoa <matsunoappy@gmail.com>
2020-06-06 20:23:06 +0900

    Fix broken build

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
 pub mod domain;
 pub mod infra;
 
+pub struct Config<'a> {
+    pub db_url: &'a str
+}
+
+pub trait Application
+where
+    Self: Sized + Clone + Send + Sync,
+{
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
-use tokio_postgres::NoTls;
-use tokio::spawn;
 use dotenv::dotenv;
-use pretty_env_logger;
 use log::{error, info};
+use pretty_env_logger;
+use tokio::spawn;
+use tokio_postgres::NoTls;
 
 use std::{collections::HashMap, env::vars, process::exit};
-
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
     dotenv().ok();
     let envs: HashMap<_, _> = vars().collect();
-    
-    let db_config  = envs.get("DATABASE_URL").unwrap_or_else(|| {
+
+    let db_config = envs.get("DATABASE_URL").unwrap_or_else(|| {
         error!("DATABASE_URL must be set.");
         exit(1);
     });
 
-    let (pg_client, pg_connection) = tokio_postgres::connect(db_config, NoTls).await.unwrap_or_else(|e| { 
-        error!("Failed to establish connection to database: {}", e);
-        exit(1);
-    });
+    let (pg_client, pg_connection) = tokio_postgres::connect(db_config, NoTls)
+        .await
+        .unwrap_or_else(|e| {
+            error!("Failed to establish connection to database: {}", e);
+            exit(1);
+        });
 
     spawn(async move {
         if let Err(e) = pg_connection.await {
@@ -29,7 +30,6 @@ async fn main() {
             exit(1);
         }
     });
-
 
     println!("Hello, world!");
 }
