bba64da7b35cfdf9ad7e3b02280c826a5d56a8da
himanoa <matsunoappy@gmail.com>
2020-06-21 20:38:28 +0900

    Delete unused variable

--- src/handlers.rs
+++ src/handlers.rs
@@ -3,7 +3,6 @@ use tokio_postgres::Client;
 use warp::{Rejection, Reply};
 
 pub async fn create_channel_handler(pg_client: Arc<Client>) -> Result<impl Reply, Rejection> {
-    let db = pg_client.clone();
     let result = pg_client
         .query(
             r#"INSERT INTO channels(channel_id, name, icon_url) VALUES ($1, $2, $3)"#,
