5d2127afaff9c8104e3a7bdfc9abe6fe34a7023e
himanoa <matsunoappy@gmail.com>
2020-07-05 06:23:03 +0900

    Delete unused user module

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -2,5 +2,4 @@ pub mod channel;
 pub mod song;
 pub mod unixtime;
 pub mod url;
-pub mod user;
 pub mod video;
--- src/domain/user/model.rs
+++
@@ -1,7 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
-pub struct User {
-    pub id: i64,
-    pub uid: String,
-}
