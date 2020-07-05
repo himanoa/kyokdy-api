9d13c724f3a5d16e6e824336cb7f69a615cf9b3e
himanoa <matsunoappy@gmail.com>
2020-07-05 22:19:44 +0900

    Use structopt

--- src/bin/import_channels_batch.rs
+++ src/bin/import_channels_batch.rs
@@ -1,6 +1,15 @@
 use dotenv::dotenv;
 use kyokdy_api::application::Application;
+use structopt::StructOpt;
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "import-channels-batch")]
+struct Opt {
+    #[structopt(short, long)]
+    dry_run: bool
+}
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
     Ok(())
 }
